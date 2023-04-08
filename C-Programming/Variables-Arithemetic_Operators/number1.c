850ef8c7f7c44c24de91e9482907d7273cb97f29
Chifund0 <chifundonyakwawa@gmail.com>
2023-04-08 22:09:19 +0200

    practice qn

---
+++ C-Programming/Variables-Arithemetic_Operators/number1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ * 
+ */
+
+int main(void)
+{
+	int length;
+	int width;
+	int area;
+
+	length = 5;
+	width = 8;
+	
+	printf("area = %d\n", length * width);
+	return (0);
+}
---
+++ C-Programming/Variables-Arithemetic_Operators/number2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int C;
+	int N;
+	int CN;
+
+	C = 9;
+	N = 4;
+	CN = C * N;
+
+	printf("CN = %d\n", C * N);
+	return (0);
+}
